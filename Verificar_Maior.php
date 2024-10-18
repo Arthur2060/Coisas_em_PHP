b8fa9fbb26f7462348e056ff15d496b43eda18bd
Arthur <arthurhenrrique1705@gmail.com>
2024-10-18 13:42:23 -0300

    Create Verificar_Maior.php

---
+++ Verificar_Maior.php
@@ -0,0 +1,12 @@
+<?php
+    $primeironumero  = 5;
+    $segundonumero   = 4;
+    
+    if($primeironumero < $segundonumero){
+        echo "$primeironumero é menor do que $segundonumero";
+    }else if($primeironumero > $segundonumero){
+        echo "$primeironumero é maior do que $segundonumero";
+    }else{
+        echo "Os dois são iguais";
+    }
+?>
